1e1c7b49ae01c1bb24e7766c33b0971f4a72a156
pavlog <pavel.grib@gmail.com>
2015-06-07 00:52:35 +0300

    !U small c# appp to visualize printer specs

---
+++ Software/SScaraVisualizer/SScaraVisualizer/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxl = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxLL = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textMaxq1 = new System.Windows.Forms.TextBox();
+            this.textMaxq2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textMinq2 = new System.Windows.Forms.TextBox();
+            this.minq2ww = new System.Windows.Forms.Label();
+            this.textMinq1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textDeltaq2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textDeltaq1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxYOffs = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxd
+            // 
+            this.textBoxd.Location = new System.Drawing.Point(608, 311);
+            this.textBoxd.Name = "textBoxd";
+            this.textBoxd.Size = new System.Drawing.Size(134, 20);
+            this.textBoxd.TabIndex = 0;
+            this.textBoxd.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(589, 314);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "d";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(583, 340);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(9, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "l";
+            // 
+            // textBoxl
+            // 
+            this.textBoxl.Location = new System.Drawing.Point(608, 337);
+            this.textBoxl.Name = "textBoxl";
+            this.textBoxl.Size = new System.Drawing.Size(134, 20);
+            this.textBoxl.TabIndex = 3;
+            this.textBoxl.Text = "70";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(583, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "L";
+            // 
+            // textBoxLL
+            // 
+            this.textBoxLL.Location = new System.Drawing.Point(608, 363);
+            this.textBoxLL.Name = "textBoxLL";
+            this.textBoxLL.Size = new System.Drawing.Size(134, 20);
+            this.textBoxLL.TabIndex = 7;
+            this.textBoxLL.Text = "80";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(855, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 27);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(514, 468);
+            this.panel2.TabIndex = 12;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(583, 404);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Max Q1";
+            // 
+            // textMaxq1
+            // 
+            this.textMaxq1.Location = new System.Drawing.Point(633, 401);
+            this.textMaxq1.Name = "textMaxq1";
+            this.textMaxq1.ReadOnly = true;
+            this.textMaxq1.Size = new System.Drawing.Size(100, 20);
+            this.textMaxq1.TabIndex = 14;
+            this.textMaxq1.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textMaxq2
+            // 
+            this.textMaxq2.Location = new System.Drawing.Point(800, 402);
+            this.textMaxq2.Name = "textMaxq2";
+            this.textMaxq2.ReadOnly = true;
+            this.textMaxq2.Size = new System.Drawing.Size(100, 20);
+            this.textMaxq2.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(750, 405);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Max Q2";
+            // 
+            // textMinq2
+            // 
+            this.textMinq2.Location = new System.Drawing.Point(800, 428);
+            this.textMinq2.Name = "textMinq2";
+            this.textMinq2.ReadOnly = true;
+            this.textMinq2.Size = new System.Drawing.Size(100, 20);
+            this.textMinq2.TabIndex = 20;
+            // 
+            // minq2ww
+            // 
+            this.minq2ww.AutoSize = true;
+            this.minq2ww.Location = new System.Drawing.Point(750, 431);
+            this.minq2ww.Name = "minq2ww";
+            this.minq2ww.Size = new System.Drawing.Size(41, 13);
+            this.minq2ww.TabIndex = 19;
+            this.minq2ww.Text = "Min Q2";
+            // 
+            // textMinq1
+            // 
+            this.textMinq1.Location = new System.Drawing.Point(633, 427);
+            this.textMinq1.Name = "textMinq1";
+            this.textMinq1.ReadOnly = true;
+            this.textMinq1.Size = new System.Drawing.Size(100, 20);
+            this.textMinq1.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(583, 430);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Min Q1";
+            // 
+            // textDeltaq2
+            // 
+            this.textDeltaq2.Location = new System.Drawing.Point(800, 458);
+            this.textDeltaq2.Name = "textDeltaq2";
+            this.textDeltaq2.ReadOnly = true;
+            this.textDeltaq2.Size = new System.Drawing.Size(100, 20);
+            this.textDeltaq2.TabIndex = 24;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(750, 461);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Delta Q2";
+            // 
+            // textDeltaq1
+            // 
+            this.textDeltaq1.Location = new System.Drawing.Point(633, 457);
+            this.textDeltaq1.Name = "textDeltaq1";
+            this.textDeltaq1.ReadOnly = true;
+            this.textDeltaq1.Size = new System.Drawing.Size(100, 20);
+            this.textDeltaq1.TabIndex = 22;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(583, 460);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Delta Q1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::SScaraVisualizer.Properties.Resources.Scheme1;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(586, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(351, 286);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(761, 317);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(33, 13);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "YOffs";
+            // 
+            // textBoxYOffs
+            // 
+            this.textBoxYOffs.Location = new System.Drawing.Point(803, 314);
+            this.textBoxYOffs.Name = "textBoxYOffs";
+            this.textBoxYOffs.Size = new System.Drawing.Size(134, 20);
+            this.textBoxYOffs.TabIndex = 25;
+            this.textBoxYOffs.Text = "20";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 483);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxYOffs);
+            this.Controls.Add(this.textDeltaq2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textDeltaq1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textMinq2);
+            this.Controls.Add(this.minq2ww);
+            this.Controls.Add(this.textMinq1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textMaxq2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textMaxq1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxLL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBoxd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxd;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxl;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxLL;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textMaxq1;
+        private System.Windows.Forms.TextBox textMaxq2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textMinq2;
+        private System.Windows.Forms.Label minq2ww;
+        private System.Windows.Forms.TextBox textMinq1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textDeltaq2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textDeltaq1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxYOffs;
+    }
+}
+
---
+++ Software/SScaraVisualizer/SScaraVisualizer/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float maxAngleLeft = -10000;
+        float maxAngleRight = -10000;
+        float minAngleLeft = 10000;
+        float minAngleRight = 10000;
+
+        public double AnglesDist(double sourceA,double targetA)
+        {
+            double a = sourceA - targetA;
+            return a;// (a + 180) % 360 - 180;
+        }
+
+        public double ConvertToDegree(double angle)
+        {
+            return (180/Math.PI) * angle;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.DrawEllipse(Pens.AliceBlue, 0, 0, 100, 100);
+            float minY = 0;
+            float maxY = 150;
+            float stepY = 1;
+            float l = float.Parse(textBoxl.Text);
+            float L = float.Parse(textBoxLL.Text);
+            float d = float.Parse(textBoxd.Text);
+            float bottomOffset = 10;
+            float stepSize = (float)Math.Floor((panel2.Size.Height - bottomOffset) / (maxY - minY));
+            float dhalf = d / 2;
+            float l2 = l * l;
+            float L2 = L * L;
+            float bedW = 100;
+            float bedH = 100;
+            float bedYOffset = float.Parse(textBoxYOffs.Text);
+            float minX = -bedW/2-5;
+            float maxX = bedW / 2 + 5;
+            float stepX = 1;
+            maxAngleLeft = -10000;
+            maxAngleRight = -10000;
+            minAngleLeft = 10000;
+            minAngleRight = 10000;
+            minY = bedYOffset-5;
+            maxY = bedYOffset+bedH+5;
+
+            //panel1.Size.Width/2+x * 5, panel1.Size.Height/2
+            ///*
+            for (float x = minX; x<maxX; x += stepX)
+            {
+            for (float y = minY; y<maxY; y += stepY)
+            {
+                NewMethod(e, l, bottomOffset, stepSize, dhalf, l2, L2, x, y,false);
+            }
+            }
+            //*/
+            //NewMethod(e, l, bottomOffset, stepSize, dhalf, l2, L2, -50, 50);
+            //NewMethod(e, l, bottomOffset, stepSize, dhalf, l2, L2, 50, 50);
+            //NewMethod(e, l, bottomOffset, stepSize, dhalf, l2, L2, -10, 5);
+            //NewMethod(e, l, bottomOffset, stepSize, dhalf, l2, L2, 10, 5);
+            e.Graphics.DrawLine(Pens.Black, new PointF(panel2.Size.Width / 2.0f, 0), new PointF(panel2.Size.Width / 2.0f, panel2.Size.Height));
+            NewMethod(e, l, bottomOffset, stepSize, dhalf, l2, L2, -50, bedYOffset + bedH, true);
+            NewMethod(e, l, bottomOffset, stepSize, dhalf, l2, L2, -50, bedYOffset, true);
+            NewMethod(e, l, bottomOffset, stepSize, dhalf, l2, L2, 50, bedYOffset + bedH, true);
+            NewMethod(e, l, bottomOffset, stepSize, dhalf, l2, L2, 50, bedYOffset, true);
+            e.Graphics.DrawRectangle(Pens.Orange, panel2.Size.Width / 2 + (-bedW / 2) * stepSize - stepSize / 2, panel2.Size.Height - 0 * stepSize - stepSize / 2 - bottomOffset - bedYOffset * stepSize - bedH * stepSize, bedW * stepSize, bedH * stepSize);
+            textMaxq1.Text = ConvertToDegree(maxAngleRight).ToString();
+            textMaxq2.Text = ConvertToDegree(maxAngleLeft).ToString();
+            textMinq1.Text = ConvertToDegree(minAngleRight).ToString();
+            textMinq2.Text = ConvertToDegree(minAngleLeft).ToString();
+            textDeltaq1.Text = ConvertToDegree(AnglesDist(maxAngleRight,minAngleRight)).ToString();
+            textDeltaq2.Text = ConvertToDegree(AnglesDist(maxAngleLeft,minAngleLeft)).ToString();
+        }
+
+        private void NewMethod(PaintEventArgs e, float l, float bottomOffset, float stepSize, float dhalf, float l2, float L2, float x, float y,bool bDrawArm)
+        {
+            float A = -2 * l * (x - dhalf);
+            float B = -2 * y * l;
+            float C = (x - dhalf) * (x - dhalf) + y * y + l2 - L2;
+            float F = (x + dhalf) * (x + dhalf) + y * y + l2 - L2;
+            float E = -2  * l * (x+dhalf);
+            float Det1 = B * B - (C * C - A * A);
+            float Det2 = B * B - (F * F - E * E);
+            Pen pen = Pens.Black;
+            if ( Det1 < 0 || Det2 < 0 )
+            {
+                pen = Pens.Red;
+            }
+            else if (Det1 == 0)
+            {
+                pen = Pens.Yellow;
+            }
+            else
+            {
+                pen = Pens.Green;
+                float qq11 = (-B - (float)Math.Sqrt(Det1)) / (C - A);
+                float q11 = 2 * (float)Math.Atan(qq11);
+                float qq12 = (-B + (float)Math.Sqrt(Det1)) / (C - A);
+                float q12 = 2 * (float)Math.Atan(qq12);
+
+                float qq21 = (-B - (float)Math.Sqrt(Det2)) / (F - E);
+                float q21 = 2 * (float)Math.Atan(qq21);
+                float qq22 = (-B + (float)Math.Sqrt(Det2)) / (F - E);
+                float q22 = 2 * (float)Math.Atan(qq22);
+
+                /*
+                if (q11 < 0)
+                {
+                    pen = Pens.Red;
+                }
+                if (q12 < 0)
+                {
+                    pen = Pens.Red;
+                }
+                */
+                float xd = -dhalf + l * (float)Math.Cos(q22);
+                float yd = l * (float)Math.Sin(q22);
+                if (q22 < 0)
+                {
+                    q22 = (float)Math.PI * 2 + q22;
+                }
+                if (q22 > maxAngleLeft)
+                {
+                    maxAngleLeft = q22;
+                }
+                if (q22 < minAngleLeft)
+                {
+                    minAngleLeft = q22;
+                }
+                ///*
+                float xb = dhalf + l * (float)Math.Cos(q11);
+                float yb = l * (float)Math.Sin(q11);
+                if ((xd-x) * (yb-y) - (yd-y) * (xb-x) < 0)
+                {
+                    pen = Pens.Blue;
+                }
+                //if (q11 < 0)
+                //{
+                 //   q11 = (float)Math.PI * 2 + q11;
+                //}
+                if (q11 > maxAngleRight)
+                {
+                    maxAngleRight = q11;
+                }
+                if (q11 < minAngleRight)
+                {
+                    minAngleRight = q11;
+                }
+                if (bDrawArm)
+                {
+                    e.Graphics.DrawLine(pen,
+                        new PointF(panel2.Size.Width / 2 + (-dhalf) * stepSize - stepSize / 2, panel2.Size.Height - 0 * stepSize - stepSize / 2 - bottomOffset),
+                        new PointF(panel2.Size.Width / 2 + (xd) * stepSize - stepSize / 2, panel2.Size.Height - yd * stepSize - stepSize / 2 - bottomOffset));
+                    e.Graphics.DrawLine(pen,
+                        new PointF(panel2.Size.Width / 2 + (x) * stepSize - stepSize / 2, panel2.Size.Height - y * stepSize - stepSize / 2 - bottomOffset),
+                        new PointF(panel2.Size.Width / 2 + (xd) * stepSize - stepSize / 2, panel2.Size.Height - yd * stepSize - stepSize / 2 - bottomOffset));
+                    e.Graphics.DrawLine(pen,
+                        new PointF(panel2.Size.Width / 2 + (dhalf) * stepSize - stepSize / 2, panel2.Size.Height - 0 * stepSize - stepSize / 2 - bottomOffset),
+                        new PointF(panel2.Size.Width / 2 + (xb) * stepSize - stepSize / 2, panel2.Size.Height - yb * stepSize - stepSize / 2 - bottomOffset));
+                    e.Graphics.DrawLine(pen,
+                        new PointF(panel2.Size.Width / 2 + (x) * stepSize - stepSize / 2, panel2.Size.Height - y * stepSize - stepSize / 2 - bottomOffset),
+                        new PointF(panel2.Size.Width / 2 + (xb) * stepSize - stepSize / 2, panel2.Size.Height - yb * stepSize - stepSize / 2 - bottomOffset));
+                }
+            }
+
+            
+            
+            e.Graphics.DrawRectangle(pen, panel2.Size.Width / 2 + x * stepSize - stepSize / 2, panel2.Size.Height - y * stepSize - stepSize / 2 - bottomOffset, stepSize, stepSize);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel2.Invalidate();
+        }
+    }
+}
